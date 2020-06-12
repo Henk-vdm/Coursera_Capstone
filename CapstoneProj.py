ff59f0d3d2cd5a86cc5d0a05755f53bfdd73c8ae
HA van der Merwe <henkvdm@has-macbook-pro.lan>
2020-06-11 22:37:05 -0500

    IBM Capstone Project

---
+++ CapstoneProj.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Applied Data Science Capstone Project Jupiter Notebook
+
+# This Notebook will be used for the Applied Data Science Capstone Project
+
+# In[1]:
+
+
+import numpy as np 
+import pandas as pd
+
+
+# In[2]:
+
+
+print ('Hello Capstone Project Course!')
+
